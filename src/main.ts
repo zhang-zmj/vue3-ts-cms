3fd1a37a98517290eefe8d3dd7e80ce391bbbbf9
zhangmj <15669993907@163.com>
2021-12-24 12:36:08 +0800

    feat(按规范搭建项目): 初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
 
-createApp(App).mount("#app");
+import { globalRegister } from "./global"
+
+import "@/network/axios_demo"
+
+const app = createApp(App)
+app.use(globalRegister)
+app.use(router)
+app.use(store)
+app.mount("#app")
---
+++ src/network/request/config.ts
@@ -0,0 +1,43 @@
+/*
+  方式一: 手动的切换不同的环境(不推荐)
+  1、开发环境:
+    const BASE_URL = 'http://coderwhy.org/dev'
+    const BASE_NAME = 'coderwhy'
+  2、生成环境:
+    const BASE_URL = 'http://coderwhy.org/prod'
+    const BASE_NAME = 'kobe'
+  3、测试环境:
+    const BASE_URL = 'http://coderwhy.org/test'
+    const BASE_NAME = 'james'
+ */
+
+/*
+  方式二: 根据process.env.NODE_ENV区分
+   * 开发环境: development
+   * 生成环境: production
+   * 测试环境: test
+*/
+
+/*
+
+  let BASE_URL = ""
+  const TIME_OUT = 10000
+  if (process.env.NODE_ENV === "production") {
+    BASE_URL = "http://coderwhy.org/prod"
+  } else if (process.env.NODE_ENV === "development") {
+    BASE_URL = "http://123.207.32.32:8000/"
+  } else {
+    BASE_URL = "http://coderwhy.org/test"
+  }
+
+  export { BASE_URL, TIME_OUT }
+
+*/
+
+/*
+
+  方式三: 第三种使用方法：
+    console.log(process.env.VUE_APP_BASE_URL)
+    console.log(process.env.VUE_APP_BASE_NAME)
+
+ */
